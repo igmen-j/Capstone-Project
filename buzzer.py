338f0fbb57321fae16fd70a49a91884c1cfffbc4
justinigmen <igmenjustin@gmail.com>
2021-01-17 15:31:52 -0600

    Testing for buzzer and ultrasonic sensor
    - Buzzer will buzz if obstacle is within 10cm of the sensor

--- buzzer.py
+++ buzzer.py
@@ -1,16 +1,15 @@
 import RPi.GPIO as GPIO
-import time import sleep
+from time import sleep
 
-#Select GPIO mode
+# Select GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-#Set buzzer - pin 23 as output
-buzzer=23
-GPIO.setup(buzzer,GPIO.OUT)
+# Set buzzer - pin 23 as output
+buzzer = 23
+GPIO.setup(buzzer, GPIO.OUT)
 
-#Run forever loop
-while True:
-    GPIO.output(buzzer,GPIO.HIGH)
-    sleep(0.5) # Delay in seconds
-    GPIO.output(buzzer,GPIO.LOW)
-    sleep(0.5)
+def buzzerSound(onOrOff):
+    if onOrOff == 1:
+        GPIO.output(buzzer, GPIO.HIGH)
+    else:
+        GPIO.output(buzzer, GPIO.LOW)
