e883d0e0276943e5da7e2a04118a26fb12ef9903
hoang22k <danny.hoang12@gmail.com>
2021-01-17 14:17:39 -0600

    Update Buzzer
    Testing PWM to reduce the volume

--- buzzer.py
+++ buzzer.py
@@ -7,10 +7,20 @@ GPIO.setmode(GPIO.BCM)
 #Set buzzer - pin 23 as output
 buzzer=23
 GPIO.setup(buzzer,GPIO.OUT)
-
+p = IO.pwm(buzzer,100)
+p.start(0)
 #Run forever loop
-while True:
-    GPIO.output(buzzer,GPIO.HIGH)
-    sleep(0.5) # Delay in seconds
-    GPIO.output(buzzer,GPIO.LOW)
-    sleep(0.5)
+
+while 1:
+    for x in range (50):
+        p.ChangeDutyCycle(x)
+        sleep(0.1)
+    for x in range (50):
+        p.ChangeDutyCycle(50-x)
+        sleep(0.1)
+
+#while True:
+#    GPIO.output(buzzer,GPIO.HIGH)
+#    sleep(0.5) # Delay in seconds
+#    GPIO.output(buzzer,GPIO.LOW)
+#    sleep(0.5)
