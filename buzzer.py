4ac54a8da027555a58e6cc2e4780dcbc278ecb78
hoang22k <danny.hoang12@gmail.com>
2021-01-17 13:54:50 -0600

    Updated Buzzer code
    Testing buzzer on pin 23

--- buzzer.py
+++ buzzer.py
@@ -1,2 +1,16 @@
 import RPi.GPIO as GPIO
-import time
+import time import sleep
+
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+#Set buzzer - pin 23 as output
+buzzer=23
+GPIO.setup(buzzer,GPIO.OUT)
+
+#Run forever loop
+while True:
+    GPIO.output(buzzer,GPIO.HIGH)
+    sleep(0.5) # Delay in seconds
+    GPIO.output(buzzer,GPIO.LOW)
+    sleep(0.5)
