962d29a562b3a5c9a037393d2fc82bdd81b5cfa8
justinigmen <igmenjustin@gmail.com>
2021-01-23 12:46:41 -0600

    Program fro three ultrasonic sensor
    - Ultrasonic sensors for left, middle, and right
    - Buzzer will buzz if obstacle is within 10cm of any of the sensors

--- buzzer.py
+++ buzzer.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
+from pinList import *
 from time import sleep
 
 # Select GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 # Set buzzer - pin 23 as output
-buzzer = 23
-GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
 def buzzerSound(onOrOff):
     if onOrOff == 1:
-        GPIO.output(buzzer, GPIO.HIGH)
+        GPIO.output(BUZZER_PIN, GPIO.HIGH)
     else:
-        GPIO.output(buzzer, GPIO.LOW)
+        GPIO.output(BUZZER_PIN, GPIO.LOW)
