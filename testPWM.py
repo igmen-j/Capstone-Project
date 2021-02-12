ac0d904aefadd84334866f673fea02c268aae1e4
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2021-02-12 17:56:04 -0600

    Update testPWM.py
    - Fixed issue with the motor driver not running when both pins are connected
    - Fix: changed the pin mode to input/output in the while loop

--- testPWM.py
+++ testPWM.py
@@ -8,18 +8,25 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 # set up GPIO pins
 GPIO.setup(12, GPIO.OUT)  # Connected to PWMA
-GPIO.setup(16, GPIO.OUT)
+#GPIO.setup(16, GPIO.OUT)
+GPIO.setup(16, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
+#GPIO.setup(18, GPIO.IN)
 
-#pwm = GPIO.PWM(12, 100)
-#DC = 0
-#pwm.start(50)
+pwm = GPIO.PWM(12, 1000)
+pwm.start(50)
 #GPIO.setup(37, GPIO.OUT)  # Connected to AIN2
 
 while True:
-    # pwm.ChangeDutyCycle(30)
+    pwm.ChangeDutyCycle(10)
+    
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.setup(18, GPIO.IN)
+    time.sleep(0.5)
+    GPIO.setup(16, GPIO.OUT)
     GPIO.output(16, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
-    # time.sleep(3)
-    #pwm.ChangeDutyCycle(30)
-
+    time.sleep(0.5)
+    GPIO.setup(16, GPIO.IN)
+    time.sleep(0.5)
