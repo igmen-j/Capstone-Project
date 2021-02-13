ec0d8d554e70fe3be0fbcca765656ee6dad25817
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2021-02-12 18:08:27 -0600

    Update pinList.py

--- pinList.py
+++ pinList.py
@@ -4,5 +4,6 @@ TRIGGER_PIN_FRONT = 17   # physical pin 11
 ECHO_PIN_FRONT = 27      # physical pin 13
 TRIGGER_PIN_RIGHT = 5    # physical pin 29
 ECHO_PIN_RIGHT = 6       # physical pin 31
-
 BUZZER_PIN = 23          # physical pin 16
+REMOTE_PIN = 26
+LED_PIN = 25
