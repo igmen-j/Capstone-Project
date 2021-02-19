ee9d9fb4c8755cb8d290eeb60df9251366ff3657
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2021-02-18 18:42:19 -0600

    Update motor.py
    - Added comments

--- motor.py
+++ motor.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python
+#=========================================================#
+# motor.py                                                #
+# File for the motor component                            #
+#=========================================================#
 
 # Import required modules
 import time
@@ -19,9 +22,17 @@ GPIO.setup(RIGHT_MOTOR_INB, GPIO.IN)
 pwm = GPIO.PWM(PWM_PIN, PWM_FREQUENCY)
 pwm.start(0)   # stop first
 
+# Function: changeDutyCycle
+# Description: Changes the duty cycle of the motors
+# Parameters: dutyCycle
+# Return: None
 def changeDutyCycle(dutyCycle):
     pwm.ChangeDutyCycle(dutyCycle)
 
+# Function: goLeft
+# Description: Moves the motor to the left
+# Parameters: None
+# Return: None
 def goLeft():
     GPIO.setup(LEFT_MOTOR_INA, GPIO.IN)
     GPIO.setup(LEFT_MOTOR_INB, GPIO.IN)
@@ -29,6 +40,10 @@ def goLeft():
     GPIO.setup(RIGHT_MOTOR_INB, GPIO.IN)
     GPIO.output(RIGHT_MOTOR_INA, GPIO.HIGH)
     
+# Function: goRight
+# Description: Moves the motor to the right
+# Parameters: None
+# Return: None
 def goRight():
     GPIO.setup(LEFT_MOTOR_INA, GPIO.OUT)
     GPIO.setup(LEFT_MOTOR_INB, GPIO.IN)
@@ -36,6 +51,10 @@ def goRight():
     GPIO.setup(RIGHT_MOTOR_INB, GPIO.IN)
     GPIO.output(LEFT_MOTOR_INA, GPIO.HIGH)
 
+# Function: goForward
+# Description: Moves the motor forward
+# Parameters: None
+# Return: None
 def goForward():
     GPIO.setup(LEFT_MOTOR_INA, GPIO.OUT)
     GPIO.setup(LEFT_MOTOR_INB, GPIO.IN)
@@ -44,6 +63,10 @@ def goForward():
     GPIO.output(LEFT_MOTOR_INA, GPIO.HIGH)
     GPIO.output(RIGHT_MOTOR_INA, GPIO.HIGH)
 
+# Function: stopMotors
+# Description: Stops all the motors
+# Parameters: None
+# Return: None
 def stopMotors():
     GPIO.setup(LEFT_MOTOR_INA, GPIO.IN)
     GPIO.setup(LEFT_MOTOR_INB, GPIO.IN)
