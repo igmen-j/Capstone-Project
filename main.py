774cad030cbaf0ef29d7c46900a44abf0face3ab
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2021-03-12 15:58:55 -0600

    Update main.py
    - Made the code more modular

--- main.py
+++ main.py
@@ -25,6 +25,50 @@ from ultrasonic import getDistance
 
 GPIO.setmode(GPIO.BCM)
 
+def isInCorrectRange(distance, distanceLeft, distanceFront, distanceRight):
+    if (distance > MIN_DISTANCE and distance < MAX_DISTANCE) and 
+    (distanceLeft > DISTANCE_TO_BUZZ and distanceFront > DISTANCE_TO_BUZZ_FRONT and distanceRight > DISTANCE_TO_BUZZ):
+        return 1
+    return 0
+
+def getPWMMultiplier(distance, position):       
+    distance_div_one = MAX_DISTANCE / 3
+    distance_div_two = MAX_DISTANCE * 2 / 3
+
+    if distance <= distance_div_one or distance >= distance_div_two:
+        distance_pwm_multiplier = 1.25
+    else:
+        distance_pwm_multiplier = 1
+
+    if position == FAR_LEFT or position == FAR_RIGHT:
+        position_pwm_multiplier = 1.5
+    elif position == MID_LEFT or position == MID_RIGHT:
+        position_pwm_multiplier = 1.25
+    else:
+        position_pwm_multiplier = 1  
+        
+    return distance_pwm_multiplier, position_pwm_multiplier
+
+def moveMotors(position):
+    if position == FAR_LEFT:
+        goLeft()
+        print("Far Left\n")
+    elif position == MID_LEFT:
+        goLeft()
+        print("Mid Left\n")
+    elif position == MIDDLE:
+        goForward()
+        print("Middle\n")
+    elif position == MID_RIGHT:
+        goRight()
+        print("Mid Right\n")
+    elif position == FAR_RIGHT:
+        goRight()
+        print("Far Right\n")
+    else:
+        stopMotors()
+    
+            
 if __name__ == '__main__':
     while True:     
         distance, position = getCamera()
@@ -38,43 +82,11 @@ if __name__ == '__main__':
         distanceRight = getDistance(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT)        
         
         # Robot moves only when user is within 1m and 2m from the robot and there is not obstacle within 20cm
-        if (distance > MIN_DISTANCE and distance < MAX_DISTANCE) and (distanceLeft > DISTANCE_TO_BUZZ and distanceFront > DISTANCE_TO_BUZZ_FRONT and distanceRight > DISTANCE_TO_BUZZ):
-            buzzerSound(BUZZER_OFF)
-            
-            distance_div_one = MAX_DISTANCE / 3
-            distance_div_two = MAX_DISTANCE * 2 / 3
-            
-            if distance <= distance_div_one or distance >= distance_div_two:
-                distance_pwm_multiplier = 1.25
-            else:
-                distance_pwm_multiplier = 1
-            
-            if position == FAR_LEFT or position == FAR_RIGHT:
-                position_pwm_multiplier = 1.5
-            elif position == MID_LEFT or position == MID_RIGHT:
-                position_pwm_multiplier = 1.25
-            else:
-                position_pwm_multiplier = 1    
-            
+        if (isInCorrectRange(distance, distanceLeft, distanceFront, distanceRight)):
+            buzzerSound(BUZZER_OFF)  
+            distance_pwm_multiplier, position_pwm_multiplier = getPWMMultiplier(distance, position)
             changeDutyCycle(DEFAULT_PWM*distance_pwm_multiplier*position_pwm_multiplier)
-            
-            if position == FAR_LEFT:
-                goLeft()
-                print("Far Left\n")
-            elif position == MID_LEFT:
-                goLeft()
-                print("Mid Left\n")
-            elif position == MIDDLE:
-                goForward()
-                print("Middle\n")
-            elif position == MID_RIGHT:
-                goRight()
-                print("Mid Right\n")
-            elif position == FAR_RIGHT:
-                goRight()
-                print("Far Right\n")
-            else:
-                stopMotors()
+            moveMotors(position)
         else:
             buzzerSound(BUZZER_ON)
             stopMotors()
